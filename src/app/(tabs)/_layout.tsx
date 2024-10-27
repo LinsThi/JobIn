425d63ac8beb5d00d7b0567da4f2db2402325ba9
LinsThi <thiagolins13255@gmail.com>
2024-10-27 17:42:50 -0300

    initial commit

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Link, Tabs } from 'expo-router';
+
+import { HeaderButton } from '@/HeaderButton';
+import { TabBarIcon } from '@/TabBarIcon';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'black',
+      }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: 'Tab One',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <HeaderButton />
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
