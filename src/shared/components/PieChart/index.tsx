e8ed3a06e41b28537569a841644e7b932ebfb79c
LinsThi <thiagolins13255@gmail.com>
2025-02-07 12:24:33 -0300

    feat: sendint piecharts changes

--- src/shared/components/PieChart/index.tsx
+++ src/shared/components/PieChart/index.tsx
@@ -3,22 +3,8 @@ import { G, Path, Svg, Text as SvgText, TSpan } from "react-native-svg";
 
 import useTheme from "~/src/shared/store/useTheme";
 
+// Função para calcular o ângulo de cada fatia com base no valor e no total
 const calculateAngle = (value: number, total: number) => (value / total) * 360;
-
-const indexAdjustment = (index: number) => {
-  switch (index) {
-    case 1:
-      return 4;
-    case 2:
-      return 5;
-    case 3:
-      return 5;
-    case 4:
-      return 2;
-    default:
-      return 5;
-  }
-};
 
 type Props = {
   dataToChart: { skill: string; count: number; color: string }[];
@@ -47,12 +33,13 @@ export function PieChart({
     <Svg width={230} height={230} viewBox="0 0 200 200">
       <G x={100} y={100}>
         {dataToChart.map((item, index) => {
-          const angle = calculateAngle(item.count, total);
+          const angle = calculateAngle(item.count, total); // Cálculo do ângulo da fatia
           const startAngle = cumulativeAngle;
           const endAngle = cumulativeAngle + angle;
 
-          const expandedRadius = selected === index ? radius * 1.1 : radius;
+          const expandedRadius = selected === index ? radius * 1.1 : radius; // Aumenta o raio da fatia selecionada
 
+          // Cálculo das coordenadas para o caminho da fatia (A e L representam o arco e a linha, respectivamente)
           const startXOuter = expandedRadius * Math.cos((Math.PI * startAngle) / 180);
           const startYOuter = expandedRadius * Math.sin((Math.PI * startAngle) / 180);
           const endXOuter = expandedRadius * Math.cos((Math.PI * endAngle) / 180);
@@ -66,22 +53,24 @@ export function PieChart({
           cumulativeAngle = endAngle;
 
           const pathData = `
-        M${startXOuter},${startYOuter}
-        A${expandedRadius},${expandedRadius} 0 ${angle > 180 ? 1 : 0} 1 ${endXOuter},${endYOuter}
-        L${endXInner},${endYInner}
-        A${innerRadius},${innerRadius} 0 ${angle > 180 ? 1 : 0} 0 ${startXInner},${startYInner} Z
-      `;
+            M${startXOuter},${startYOuter}
+            A${expandedRadius},${expandedRadius} 0 ${angle > 180 ? 1 : 0} 1 ${endXOuter},${endYOuter}
+            L${endXInner},${endYInner}
+            A${innerRadius},${innerRadius} 0 ${angle > 180 ? 1 : 0} 0 ${startXInner},${startYInner} Z
+          `;
 
-          const labelRadius =
-            innerRadius +
-            (expandedRadius - innerRadius) * Math.max((item.count / total) * 2, 0.4) +
-            indexAdjustment(index);
+          // Cálculo do ângulo central para a label
+          const centralAngle = (startAngle + endAngle) / 2;
 
-          const labelX = labelRadius * Math.cos((Math.PI * (startAngle + endAngle)) / 2 / 180);
-          const labelY = labelRadius * Math.sin((Math.PI * (startAngle + endAngle)) / 2 / 180);
+          // const labelRadius =
+          //   innerRadius + (expandedRadius - innerRadius) * Math.max((item.count / total) * 2, 0.4); // Define a distância do centro ao texto
+          const labelRadius = radius - 10;
+          console.log(item.skill, (item.count / total) * 2);
+          const labelX = labelRadius * Math.cos((Math.PI * centralAngle) / 180); // Calcula a coordenada X para a label
+          const labelY = labelRadius * Math.sin((Math.PI * centralAngle) / 180); // Calcula a coordenada Y para a label
 
+          // Dividir o nome da habilidade caso tenha " de " para quebrar em partes
           let parts;
-
           if (item.skill.includes(" de ")) {
             parts = String(item.skill).split(" de");
             parts[0] += " de";
@@ -93,7 +82,7 @@ export function PieChart({
             <G key={index} onPressIn={() => onPressSlice(index)}>
               <Path
                 d={pathData}
-                fill={selected === index || selected === null ? item.color : item.color + "50"}
+                fill={selected === index || selected === null ? item.color : item.color + "50"} // Aplica cor, com opacidade quando não selecionado
                 stroke={selected === index ? "#666" : "none"}
                 strokeWidth={selected === index ? 1 : 0}
               />
@@ -105,7 +94,7 @@ export function PieChart({
                     textAnchor="middle"
                     alignmentBaseline="middle"
                     fontSize={12}
-                    fill="#FFFFFF"
+                    fill={theme === "dark" ? "#FFFFFF" : "#000000"}
                     fontWeight="bold">
                     {parts.map((part, idx) => (
                       <TSpan key={idx} x={0} dy={idx > 0 ? 14 : 0}>
--- src/shared/utils/chart_colors.tsx
+++ src/shared/utils/chart_colors.tsx
@@ -4,28 +4,20 @@ import { RankedSkillProps } from "~/src/shared/queries/useQueryGetVacantionsAddR
 
 export const themeColors = {
   light: [
-    "#0082FB",
-    "#6A90B4",
-    "#5AA4E8",
-    "#85ADCC",
-    "#94A9BC",
-    "#28323B",
-    "#42576D",
-    "#A2C5E5",
-    "#546F8A",
-    "#293D50",
+    "#00B0FF", // Azul brilhante
+    "#FF4081", // Rosa vibrante
+    "#00C853", // Verde vibrante
+    "#FF9100", // Amarelo saturado
+    "#D500F9", // Roxo intenso
+    "#0038B7", // Azul escuro
   ],
   dark: [
-    "#8A31FF",
-    "#8B6BB5",
-    "#985AE8",
-    "#806B9C",
-    "#736782",
-    "#E3D8F2",
-    "#BFAED6",
-    "#A68DC7",
-    "#5B4875",
-    "#4D376B",
+    "#2979FF", // Azul profundo
+    "#FF4081", // Rosa intenso
+    "#7C4DFF", // Roxo elétrico
+    "#FF6D00", // Laranja brilhante
+    "#1DE9B6", // Verde água brilhante
+    "#4A2186", // Roxo escuro
   ],
 };
 
@@ -36,10 +28,10 @@ export const useFormattedSkillsWithColors = (
   const colorMapRef = useRef(new Map<string, string>());
 
   if (colorMapRef.current.size === 0) {
-    skillsObject.forEach((skill) => {
+    skillsObject.forEach((skill, index) => {
       if (!colorMapRef.current.has(skill.skill)) {
-        const randomIndex = Math.floor(Math.random() * themeColors.light.length);
-        colorMapRef.current.set(skill.skill, themeColors.light[randomIndex]);
+        const lightColor = themeColors.light[index];
+        colorMapRef.current.set(skill.skill, lightColor);
       }
     });
   }
