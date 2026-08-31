137db6712fc8c3beca09121229168c85ca48be45
LinsThi <thiagolins13255@gmail.com>
2026-08-31 20:38:04 -0300

    package: updating packages versions

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = ["react-native-reanimated/plugin"];
 
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-
-    plugins,
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          unstable_transformImportMeta: true, // 👈 ESSENCIAL
+        },
+      ],
+      "nativewind/babel",
+    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
